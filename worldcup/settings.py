d9393b16e7927e80ffb2a3b1088c30e1bf6ee65b
miyizhang <112915938+miyizhang@users.noreply.github.com>
2024-03-24 16:49:11 +0800

    Changes

--- worldcup/settings.py
+++ worldcup/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -25,8 +24,8 @@ SECRET_KEY = "django-insecure-bbvz-oz8dbfu@2dvbj#714&fhxvypzz!^gecy_ae)bjque@z52
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['7fd0b960.r10.cpolar.top', 'wordcup.services', 'www.worldcup.services', '0.0.0.0','aiun.top', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['7fd0b960.r10.cpolar.top', 'worldcup.services', 'www.worldcup.services', '0.0.0.0', 'aiun.top',
+                 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -70,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "worldcup.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,7 +78,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -100,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -111,7 +107,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
@@ -129,4 +124,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGOUT_REDIRECT_URL = '/home/'
 
 LOGIN_REDIRECT_URL = 'home'
-
